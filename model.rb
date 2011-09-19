4525c50183da27a22548acbc2f5526fafcaecf71
Barry Frost <public@barryfrost.com>
2011-09-19 11:04:31 +0100

    Try using PG locally.

--- model.rb
+++ model.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'active_record'
 
-dbconfig = YAML.load_file('./config/database.yml')
+dbconfig = YAML.load(File.read('config/database.yml'))
 ActiveRecord::Base.establish_connection dbconfig[settings.environment.to_s]
 
 class Movie < ActiveRecord::Base; end
